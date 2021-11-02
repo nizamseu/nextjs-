35c466736d131e7b53e978f994f0fcbb1baaaec0
Nizam Uddin Babu <nizamcse.seu@gmail.com>
2021-11-03 00:37:44 +0600

    post API is Done

--- pages/api/userapi.js
+++ pages/api/userapi.js
@@ -6,5 +6,5 @@ export default async function handler(req,res){
  const {db} = await connectToDatabase();
 const data = await db.collection("users").find({}).toArray();
 console.log(data);
-    res.json("heool")
+    res.json(data)
 }
